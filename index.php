97a0360faa8a759af76464c57bdf6648f1345e19
stnrtch1 <stnrtch1@gmail.com>
2020-10-18 15:16:41 -0300

    Added dummy table data for format planning

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,7 +16,41 @@
   </head>
   <body class="pt-3">
     <div class="container">
+
       <h1>Hello, world!</h1>
+
+      <table class="table table-striped table-hover">
+        <thead class="thead-dark">
+          <tr>
+            <th>Game</th>
+            <th>Achievements Earned</th>
+            <th>Achievement Total</th>
+            <th>Percentage Earned</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Crash Bandicoot 3</td>
+            <td>26</td>
+            <td>26</td>
+            <td>100%</td>
+          </tr>
+          <tr>
+            <td>Spyro 3</td>
+            <td>5</td>
+            <td>50</td>
+            <td>10%</td>
+          </tr>
+          <tr>
+            <td>Payday 2</td>
+            <td>305</td>
+            <td>1000</td>
+            <td>30.5%</td>
+          </tr>
+        </tbody>
+      </table>
+
+      
 
       <div class="container-sm">100% wide until small breakpoint</div>
       <div class="container-md">100% wide until medium breakpoint</div>
@@ -20,8 +58,6 @@
       <div class="container-xl">100% wide until extra large breakpoint</div>
     </div>
     
-
-
     <!-- JS Scripts needed for Bootstrap-->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
