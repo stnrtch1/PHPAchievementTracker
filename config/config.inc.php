fd967f80b63353da5288bf09179505b86f0b4381
stnrtch1 <stnrtch1@gmail.com>
2021-01-07 13:50:58 -0400

    Created database config

---
+++ config/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+    //database info
+    $config = array(
+        'DB_HOST'=>'localhost',
+        'DB_USERNAME'=>'root',
+        'DB_PASSWORD'=>'',
+        'DB_DATABASE'=>'achievementtracker',
+        'DB_GAMETABLE'=>'games',
+        
+    );
