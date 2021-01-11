428e674f559274ed2c4f4020bf0eefd99748823d
stnrtch1 <stnrtch1@gmail.com>
2021-01-11 14:12:05 -0400

    Added Form Validation

--- addgame.php
+++ addgame.php
@@ -3,6 +3,46 @@
         print_r($_POST);
     }
 
+    //GO BACK
+    //if "Go Back" was hit, return to index.php
+    if(isset($_POST['back'])){
+        header('location:/index.php');
+        exit;
+    }
+
+    //setup database config
+    include 'config/config.inc.php';
+    $DB_HOST = $config['DB_HOST'];
+    $DB_USERNAME = $config['DB_USERNAME'];
+    $DB_PASSWORD = $config['DB_PASSWORD'];
+    $DB_DATABASE = $config['DB_DATABASE'];
+    $DB_GAMETABLE = $config['DB_GAMETABLE'];
+
+    //now, connect to the database
+    $connection = mysqli_connect($DB_HOST,$DB_USERNAME,$DB_PASSWORD)
+    or die(mysqli_error($connection));
+    $db = mysqli_select_db($connection,$DB_DATABASE) or die(mysqli_error($connection));
+
+    //SUBMIT
+    //if "Submit" was hit, check all fields and then proceed to add the game
+    if(isset($_POST['submit'])){
+        //check all inputs if they are good
+
+        //is game name empty or not?
+        if($_POST['gameName'] != ""){
+            //is the max achievements field greater or equal to the achievements earned field
+            if($_POST['gameMaxAchievements'] >= $_POST['gameAchievementsEarned']){
+                print "<br>Game is good to go!";
+            }else{
+                $errorMessage = "Might want to double check your numbers, the max number field is smaller than the earned field.";
+            }
+        }else{
+            $errorMessage = "Game Name Field is empty!";
+        }
+    }
+    
+    
+
 
 ?>
 
@@ -36,9 +76,15 @@
                 <div id="gameAchievementsEarnedHelp" class="form-text">The amount of achievements you earned. If you didn't earn any yet, that's okay. Just keep it at zero.</div>
             </div>
             <div class="mb-3">
-                <label for="gameMaxAchievements" class="form-label">Achievements Earned</label>
+                <label for="gameMaxAchievements" class="form-label">Max Achievements</label>
                 <input type="number" min="1" value="1" class="form-control" id="gameMaxAchievements" name="gameMaxAchievements" aria-describedby="gameMaxAchievementsHelp">
                 <div id="gameMaxAchievementsHelp" class="form-text">The amount of achievements for the game. Unlike the other section, this one can't be zero.</div>
+            </div>
+
+            <div class="mb-3">
+                <?php if(isset($errorMessage)){ ?>
+                    <div class="text-danger"><?=$errorMessage?></div>
+                <?php } ?>
             </div>
 
             <button type="submit" name="submit" class="btn btn-primary">Submit</button>
